a00745cd8c64520d5e12411549872804f22f26f7
ntalbs <ntalbsen@gmail.com>
2020-07-06 13:23:26 +0100

    Move twitter tokens to SecretsManager

--- lambdas/tweetbook/index.js
+++ lambdas/tweetbook/index.js
@@ -1,20 +1,30 @@
 const aws = require('aws-sdk')
 const dynamodb = new aws.DynamoDB.DocumentClient({apiVersion: '2012-08-10'})
+const secretsManager = new aws.SecretsManager({ region: 'eu-west-1' })
 const Twit = require('twit')
 
-const T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
+async function getTokens () {
+  let secrets = await secretsManager.getSecretValue({SecretId: 'TwitterBookBotApiTokens'}).promise()
+  return JSON.parse(secrets.SecretString)
+}
 
-function tweet(quote) {
+let T;
+async function tweet(quote) {
+  if (!T) {
+    let tokens = await getTokens();
+    T = new Twit({
+      consumer_key: tokens.TWITTER_CONSUMER_KEY,
+      consumer_secret: tokens.TWITTER_CONSUMER_SECRET,
+      access_token: tokens.TWITTER_ACCESS_TOKEN,
+      access_token_secret: tokens.TWITTER_ACCESS_TOKEN_SECRET
+    })
+  }
+
   let msg = `${quote.msg}\n${quote.src}`
   return T.post('statuses/update', {status: msg})
 }
 
-const MAX_INDEX = 553
+const MAX_INDEX = 551
 
 function random() {
   return Math.floor(Math.random() * MAX_INDEX)
