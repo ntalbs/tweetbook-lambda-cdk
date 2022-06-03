e40b22eedc963371ad606b66ac342d68ebdeac41
ntalbs <ntalbsen@gmail.com>
2022-06-03 07:49:20 +0100

    Migrate to cdk v2

--- bin/tweetbook.js
+++ bin/tweetbook.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
+const cdk = require('aws-cdk-lib');
 const { TweetbookStack } = require('../lib/tweetbook-stack');
 
 const app = new cdk.App();
